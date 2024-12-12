5985fbda46233d8c3c024c61a4c8e4bef073e024
Arina <wolf.arishka@gmailcom>
2024-12-12 19:57:11 +0300

    add actor's cards

--- solaris-next-app/app/page.js
+++ solaris-next-app/app/page.js
@@ -3,12 +3,14 @@ import styles from "./page.module.css";
 import Banner from './components/Banner';
 import MusicBlock from './components/MusicBlock';
 import VideoBlock from './components/VideoBlock';
+import { Actors } from "./components/Actors";
 
 export default function Home() {
   return (
     <div>
       <VideoBlock />
       <Banner />
+      <Actors />
       <MusicBlock />
       {/* Другие секции сайта */}
     </div>
